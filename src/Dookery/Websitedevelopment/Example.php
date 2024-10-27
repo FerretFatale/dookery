3b86fe983ddc87f9d3400b52d500ce78c97640e8
ferretfatale <tawneylc@hotmail.com>
2024-10-28 08:53:15 +1100

    Resolved merge conflicts in autoload files

---
+++ src/Dookery/Websitedevelopment/Example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dookery\Websitedevelopment;
+
+class Example {
+    public function sayHello() {
+        return "Hello from Dookery!";
+    }
+}
+
+
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dookery\Websitedevelopment\Example;
+
+$example = new Example();
+echo $example->sayHello();
+
